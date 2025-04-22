b1b4d83ed0896cff8be769dd08ed14adf71453d0
Olga Svetlichnaya | Tesena <olga.svetlichnaya@tesena.com>
2025-04-22 19:16:50 +0200

    Konec asserts

--- src/pages/pmtool/dashboard_page.ts
+++ src/pages/pmtool/dashboard_page.ts
@@ -1,4 +1,4 @@
-import { type Locator, type Page } from "@playwright/test";
+import { expect, type Locator, type Page } from "@playwright/test";
 import { LoginPage } from "./login_page.ts";
 import { ProjectsPage } from "./projects_page.ts";
 
@@ -8,12 +8,14 @@ export class DashboardPage {
   private readonly profileButton: Locator;
   private readonly logoutButton: Locator;
   private readonly projectsButton: Locator;
+  private readonly pageNavBar: Locator;
   //constructor v kterem nastavime jednotlive lokatory
   constructor(page: Page) {
     this.page = page;
     this.profileButton = page.locator("#user_dropdown");
     this.logoutButton = page.locator("#logout");
     this.projectsButton = page.locator("#Projects a");
+    this.pageNavBar = page.locator(".navbar-brand");
   }
   //ovladaci metody
   async clickProfile(): Promise<DashboardPage> {
@@ -30,5 +32,15 @@ export class DashboardPage {
   async clickProjects(): Promise<ProjectsPage> {
     await this.projectsButton.click();
     return new ProjectsPage(this.page);
+  }
+
+  async profileButtonIsVisible(): Promise<DashboardPage> {
+    await expect(this.profileButton).toBeVisible();
+    return this;
+  }
+
+  async appHeaderHasText(appName: string): Promise<DashboardPage> {
+    await expect(this.pageNavBar).toHaveText(appName);
+    return this;
   }
 }
