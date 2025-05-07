fb7d46a8b75d257447de0f228dcb5810ce364a8a
Olga Svetlichnaya | Tesena <olga.svetlichnaya@tesena.com>
2025-05-07 17:39:01 +0200

    Comit lekce z 6.5 před zahajenim DU

---
+++ tests/exercises/api_request_body_exercise.spec.ts
@@ -0,0 +1,17 @@
+import { test } from "@playwright/test";
+import { faker } from "@faker-js/faker";
+
+test("POST Request with Body", async ({ request }) => {
+  const username = faker.internet.username();
+  const email = faker.internet.exampleEmail();
+  await request.post(
+    "https://tegb-backend-877a0b063d29.herokuapp.com/eshop/register",
+    {
+      data: {
+        username,
+        password: "123456",
+        email,
+      },
+    }
+  );
+});
